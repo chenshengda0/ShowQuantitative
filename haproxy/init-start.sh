3295da3d10556bba21692170c648635b29d5b6fb
chenshengdawin10 <chenshengdawin10@yeah.net>
2022-10-17 15:59:52 +0800

    add

--- haproxy/init-start.sh
+++ haproxy/init-start.sh
@@ -6,7 +6,7 @@ ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 #echo -e "Y\n" | unminimize
 #设置环境变量
 for item in $(cat /proc/1/environ | tr '\0' '\n');do echo "export ${item}" >>  /etc/environment;done
-sed -i "$ a source /etc/environment" ~/.bashrc
+sed -i "$ a source /etc/environment" /root/.bashrc
 
 echo 0
 #启动服务,启动插件
@@ -36,7 +36,7 @@ rabbitmqctl  set_permissions -p / information ".*" ".*" ".*"
 
 #执行screen
 screen -dmS haproxyServer
-screen -x -S haproxyServer -p 0 -X stuff $'haproxy -f /opt/haproxy.cnf'
+screen -x -S haproxyServer -p 0 -X stuff $'sudo haproxy -f /opt/haproxy.cnf'
 screen -x -S haproxyServer -p 0 -X stuff $'\n'
 
 echo "init-start end"
